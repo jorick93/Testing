456c5e3b6c0f57c87f33b468e50f1a30f6698eba
jorick93 <74463968+jorick93@users.noreply.github.com>
2021-08-20 14:56:57 +0200

    working version

--- randomStringGenerator.py
+++ randomStringGenerator.py
@@ -1,5 +1,10 @@
 import string
-def randomString(size: int, **kwargs):
+import random
+
+def devide(num,div):
+    return [num // div + (1 if x < num % div else 0)  for x in range (div)]
+
+def randomString(size: int, *args, **kwargs):
     # Default returns only lowercase letters
     # Option: "upercase":           0 = no uppercase(default),   1 = random uppercase
     # Option: "numbers":            0 = no numbers(default),     1 = random numbers
@@ -7,44 +12,53 @@ def randomString(size: int, **kwargs):
     # Option: "diacritics"          0 = no diacritics(default),  1 = random diacritics # TODO
     # Option: "exclude"             Excludes all given karakters in the result.
     
-    options = 0 # Default lowercasess
-    lowercase, upercase,numbers,symbol,diacritics, exclude = "","","","","",""
-
-
-
-    if "lowercase" in kwargs: 
+    options = 0
+    stringlist = []
+    exclude = ""
+    if "exclude" in kwargs:
+        exclude = kwargs.get("exclude")
+    if "lowercase" in args: 
         options += 1
         lowercase = string.ascii_lowercase
         for x in exclude:
-            lowercase.replace(x,"")        
-    if "upercase" in kwargs: 
+            lowercase = lowercase.replace(x,"")
+        stringlist.append(lowercase)
+    if "uppercase" in args: 
         options += 1
-        upercase = string.ascii_uppercase
+        uppercase = string.ascii_uppercase
         for x in exclude:
-            upercase.replace(x,"")
-    if "numbers" in kwargs:
+            uppercase = uppercase.replace(x,"")
+        stringlist.append(uppercase)
+    if "digits" in args:
         options += 1
-        numbers = string.digits
+        digits = string.digits
         for x in exclude:
-            numbers.replace(x,"")
-    if "specials" in kwargs:
+            digits = digits.replace(x,"")
+        stringlist.append(digits)
+    if "symbol" in args:
         options += 1
         symbol = string.punctuation
         for x in exclude:
-            symbol.replace(x,"")
-    # if "diacritics" in kwargs:
-    #     options += 1
+            symbol = symbol.replace(x,"")
+        stringlist.append(symbol)
+    if "diacritics" in args:
+        raise NotImplementedError
 
-    # Error for no or to much options
+    # Error to much or no options
     if options == 0:
         raise Exception("No available options selected")
     if options > size:
         raise Exception("Size not sufficient with options.")
-
     
-
-    return output
+    devision = devide(size,options)
+    part = 0
+    characters = []
+    for option in stringlist:
+        characters.extend(random.choices(option,k=devision[part]))
+        random.shuffle(characters)
+        part += 1
+    return "".join(map(str,characters))
 
 
 if __name__ == '__main__':
-    print(randomString(8))
+    print(randomString(8, "lowercase", "uppercase","symbol",exclude="abcdefghi"))
